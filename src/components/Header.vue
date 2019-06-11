bc1fa02ffc52e423a5dd6f2f3b486d35c27a501c
camel9833 <camel9833@daum.net>
2019-06-11 13:14:55 +0900

    1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,7 @@
 <template>
 <div class="header">
   <nav>
-<<<<<<< HEAD
-    <a>local22</a>
-=======
     <a>master</a>
->>>>>>> 22f331e25ec5bab6e93d020eb0e640861a673507
     <a>Home</a>
     <a>Posts</a>
     <a>Contact</a>
