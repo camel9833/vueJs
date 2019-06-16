69c15ecae57e3632b75060d6942602fdeecc66e4
camel9833 <camel9833@daum.net>
2019-06-17 05:51:52 +0900

    111

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,43 @@
 <template>
 <div class="header">
-  <h3>1. header (master)</h3>
+
+  <h4> header  </h4>
+
   <nav>
-    <a>Home</a>
-    <a>Posts</a>
-    <a>Contact</a>
+    <td v-for='list in linklist'>
+      <router-link :to='list.link'>{{list.content}}</router-link>&nbsp;&nbsp;&nbsp;
+    </td>
   </nav>
+
 </div>
 </template>
 
 <script>
 export default {
+
+  data() {
+    return {
+      linklist: [{
+          content: 'Home',
+          link: '/'
+        },
+        {
+          content: 'Sample',
+          link: '/sample'
+        },
+        {
+          content: 'Mixin',
+          link: '/mixintest'
+        },
+        {
+          content: 'Extend',
+          link: '/sampleextend'
+        }
+      ]
+
+    }
+  }
+
 
 }
 </script>
