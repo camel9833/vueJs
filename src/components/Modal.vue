94bdbf95b507ba9d06a83686b6deb6baee46d250
camel9833 <camel9833@daum.net>
2019-06-22 06:37:38 +0900

    1

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,29 +1,34 @@
 
 <template>
-    <div class="row">
-        <modals-container />
-    </div>
+  <div class="row">
+    <modals-container />
+  </div>
 </template>
 
 <script>
-import DelPopup from './Modalpopup.vue'
+import DelPopup from "./Modalpopup.vue";
 export default {
-mounted(){
-        this.doc_del_rendar();
-     
-    },methods : {
-doc_del_rendar(){
-                this.$modal.show(DelPopup,{
-                    hot_table : 'data',
-                    modal : this.$modal },{
-                        name: 'dynamic-modal',
-                        width : '330px',
-                        height : '130px',
-                        draggable: true,
-            })
-}
-}
-}
+  mounted() {
+    this.doc_del_rendar();
+  },
+  methods: {
+    doc_del_rendar() {
+      this.$modal.show(
+        DelPopup,
+        {
+          hot_table: "data",
+          modal: this.$modal
+        },
+        {
+          name: "dynamic-modal",
+          width: "330px",
+          height: "130px",
+          draggable: true
+        }
+      );
+    }
+  }
+};
 </script>
 
     
